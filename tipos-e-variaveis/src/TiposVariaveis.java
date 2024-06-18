886cbc12ed63584f09c39ba3e4b38c119ebd1e07
iduque <ideduque17@gmail.com>
2024-06-17 22:20:04 -0300

    aprendendo tipos de dados

--- tipos-e-variaveis/src/TiposVariaveis.java
+++ tipos-e-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class TiposVariaveis{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Tipos primitivos
+
+        String meuNome = "Joseilde Duque";
+        double salarioMinimo = 2500;
+       
+        short numeroCurto = 1;
+        int numeroNormal1 = numeroCurto;
+
+        int numero = 5;
+        numero = 10;
+
+        System.out.println(numero);
+
+        // Constantes
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
