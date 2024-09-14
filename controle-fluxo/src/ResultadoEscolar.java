1707323f60862d0b576eb41969b6696949d0bb3d
iduque <ideduque17@gmail.com>
2024-09-14 19:49:07 -0300

    Aprendendo sobre Switch Case

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -6,7 +6,7 @@ public class ResultadoEscolar {
          int nota = 6;
          String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
          System.out.println("resultado");
-        
+    
         /*EX 1
         int nota = 7;
         String resultado = nota >=7 ? "Aprovado" : "Reprovado";
@@ -14,18 +14,18 @@ public class ResultadoEscolar {
         */
        
 
-      /*  // usando if e else
-       
-      int nota = 10;
-       
-       if(nota >= 7)
-        System.out.println("Aprovado");
+        /*  // usando if e else
+        
+        int nota = 10;
+        
+        if(nota >= 7)
+            System.out.println("Aprovado");
 
-        else if (nota >= 5 && nota < 7)
-        System.out.println("Prova de recuperação");
+            else if (nota >= 5 && nota < 7)
+            System.out.println("Prova de recuperação");
 
-       else
-        System.out.println("Reprovado");
-        */
+        else
+            System.out.println("Reprovado");
+            */
     }
 }
