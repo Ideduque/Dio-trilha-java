0d48c1c07e9bf742a3d50e23d588afc21240f5b4
iduque <ideduque17@gmail.com>
2024-09-14 20:29:22 -0300

    exemplificando o uso do switch case

--- controle-fluxo/src/PlanoOperadora.java
+++ controle-fluxo/src/PlanoOperadora.java
@@ -1,5 +1,41 @@
 public class PlanoOperadora {
+    @SuppressWarnings("unused")
     public static void main(String[] args) {
+       //USANDO SWITCH CASE
+       
+        String plano = "M"; // B = Basico, M = Midia, T = Turbo
         
+        switch (plano) {
+            case "T": {
+                System.out.println("5GB YouTube");
+            }
+            case "M": {
+                System.out.println("whats e Instagram grátis");
+            }
+            case "B": {
+                System.out.println("100 minutos de ligação");
+            }
+        }
     }
 }
+
+//USANDO IF 
+
+/* 
+public class PlanoOperadora {
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        String plano = "M"; // B = Basico, M = Midia, T = Turbo
+        
+        if(plano == "B") {
+            System.out.println("100 minutos de ligação");
+        } else if(plano == "M") {
+            System.out.println("100 minutos de ligação");
+            System.out.println("whats e Instagram grátis");
+        } else if(plano == "T") {
+            System.out.println("100 minutos de ligação");
+            System.out.println("whats e Instagram grátis");
+            System.out.println("5GB YouTube");
+        }
+    }
+}*/
