a88c04f19ab5edd8337038b806e0354c2b8566c4
iduque <ideduque17@gmail.com>
2024-09-14 20:01:07 -0300

    aplicando exemplo sobre if e switch case

--- controle-fluxo/src/SistemaMedida.java
+++ controle-fluxo/src/SistemaMedida.java
@@ -1,5 +1,8 @@
 public class SistemaMedida {
     public static void main(String[] args) {
+        //Exemplos para diferenciar o uso do IF e Switch case
+        
+         //USO DO IF
         String sigla = "P";
 
         if(sigla == "P")
@@ -10,5 +13,29 @@ public class SistemaMedida {
                 System.out.println("GRANDE");
         else
                 System.out.println("INDEFINIDO");
+                
     }
+
+    /* USANDO O SWITCH CASE 
+    
+String sigla = "M";
+
+switch (sigla){
+    case "P":{
+        System.out.println("PEQUENO");
+        break;
+    }
+    case "M":{
+        System.out.println("MEDIO");
+        break;
+    }
+    case "G":{
+        System.out.println("GRANDE");
+        break;
+    }
+    default:
+        System.out.println("INDEFINIDO");
 }
+*/
+}
+     
