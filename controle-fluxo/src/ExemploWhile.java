75d32a72be96f221df4e4efe030c759e5864f6cb
iduque <ideduque17@gmail.com>
2024-10-02 13:18:35 -0300

    Exemplo do uso While

--- controle-fluxo/src/ExemploWhile.java
+++ controle-fluxo/src/ExemploWhile.java
@@ -2,6 +2,24 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public  class ExemploWhile {
     public static void main(String[] args) {
+        double mesada = 50.0;
         
+        while(mesada > 0) 
+        {
+            double valorDoce = valorAleatorio();
+            //verificação para não precisar fazer novas verificações de valores e não deixar o valor negativo
+            if(valorDoce > mesada)
+                valorDoce = mesada; 
+            
+            System.out.println("Doce do valor: " + valorDoce + " Adicionado no carrinho");
+            mesada = mesada - valorDoce;
+        } 
+        System.out.println("Mesada:" + mesada);
+        System.out.println("João gastou toda sua mesada em doce");
+        
+    }
+    // Método para auxiliar no valor aleatorio
+    private static double valorAleatorio(){
+        return ThreadLocalRandom.current().nextDouble(2,15);
     }
 }
